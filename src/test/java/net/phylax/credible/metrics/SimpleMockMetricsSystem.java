d9432c06df06ded1f73fcbdbc9b82aa8e448b0dd
penumbra23 <glbranimir@gmail.com>
2025-09-29 14:36:19 +0200

    tests: add mock for metrics system

---
+++ src/test/java/net/phylax/credible/metrics/SimpleMockMetricsSystem.java
@@ -0,0 +1,130 @@
+package net.phylax.credible.metrics;
+
+import java.util.Set;
+import java.util.function.IntSupplier;
+
+import org.hyperledger.besu.plugin.services.MetricsSystem;
+import org.hyperledger.besu.plugin.services.metrics.Counter;
+import org.hyperledger.besu.plugin.services.metrics.Histogram;
+import org.hyperledger.besu.plugin.services.metrics.LabelledMetric;
+import org.hyperledger.besu.plugin.services.metrics.LabelledSuppliedMetric;
+import org.hyperledger.besu.plugin.services.metrics.LabelledSuppliedSummary;
+import org.hyperledger.besu.plugin.services.metrics.MetricCategory;
+import org.hyperledger.besu.plugin.services.metrics.OperationTimer;
+import com.google.common.cache.Cache;
+
+public class SimpleMockMetricsSystem implements MetricsSystem {
+    
+    private int timerCreationCount = 0;
+    private int counterCreationCount = 0;
+    private int gaugeCreationCount = 0;
+    
+    @Override
+    public LabelledMetric<Counter> createLabelledCounter(
+            MetricCategory category, String name, String help, String... labelNames) {
+        counterCreationCount++;
+        return new MockLabelledCounter();
+    }
+    
+    @Override
+    public LabelledMetric<OperationTimer> createLabelledTimer(
+            MetricCategory category, String name, String help, String... labelNames) {
+        timerCreationCount++;
+        return new MockLabelledTimer();
+    }
+    
+    @Override
+    public void createIntegerGauge(
+            MetricCategory category, String name, String help, IntSupplier supplier) {
+        gaugeCreationCount++;
+    }
+    
+    public int getTimerCreationCount() { return timerCreationCount; }
+    public int getCounterCreationCount() { return counterCreationCount; }
+    public int getGaugeCreationCount() { return gaugeCreationCount; }
+    
+    // Mock implementations of metrics
+    private static class MockLabelledCounter implements LabelledMetric<Counter> {
+        private final MockCounter counter = new MockCounter();
+        
+        @Override
+        public Counter labels(String... labelValues) {
+            return counter;
+        }
+    }
+    
+    private static class MockLabelledTimer implements LabelledMetric<OperationTimer> {
+        private final MockOperationTimer timer = new MockOperationTimer();
+        
+        @Override
+        public OperationTimer labels(String... labelValues) {
+            return timer;
+        }
+    }
+    
+    private static class MockCounter implements Counter {
+        private long count = 0;
+        
+        @Override
+        public void inc() {
+            count++;
+        }
+    
+        @Override
+        public void inc(long amount) {
+            count += amount;
+        }
+    }
+    
+    private static class MockOperationTimer implements OperationTimer {
+        private int startCount = 0;
+        
+        @Override
+        public TimingContext startTimer() {
+            startCount++;
+            return () -> { return startCount; };
+        }
+    }
+
+    // NOTE: unused methods
+    @Override
+    public void createGuavaCacheCollector(MetricCategory arg0, String arg1, Cache<?, ?> arg2) {
+        throw new UnsupportedOperationException("Unimplemented method 'createGuavaCacheCollector'");
+    }
+
+    @Override
+    public LabelledMetric<Histogram> createLabelledHistogram(MetricCategory arg0, String arg1, String arg2,
+            double[] arg3, String... arg4) {
+        throw new UnsupportedOperationException("Unimplemented method 'createLabelledHistogram'");
+    }
+
+    @Override
+    public LabelledSuppliedMetric createLabelledSuppliedCounter(MetricCategory arg0, String arg1, String arg2,
+            String... arg3) {
+        throw new UnsupportedOperationException("Unimplemented method 'createLabelledHistogram'");
+    }
+
+    @Override
+    public LabelledSuppliedMetric createLabelledSuppliedGauge(MetricCategory arg0, String arg1, String arg2,
+            String... arg3) {
+        throw new UnsupportedOperationException("Unimplemented method 'createLabelledSuppliedGauge'");
+    }
+
+    @Override
+    public LabelledSuppliedSummary createLabelledSuppliedSummary(MetricCategory arg0, String arg1, String arg2,
+            String... arg3) {
+        throw new UnsupportedOperationException("Unimplemented method 'createLabelledSuppliedSummary'");
+    }
+
+    @Override
+    public LabelledMetric<OperationTimer> createSimpleLabelledTimer(MetricCategory arg0, String arg1, String arg2,
+            String... arg3) {
+        return new MockLabelledTimer();
+    }
+
+    @Override
+    public Set<MetricCategory> getEnabledCategories() {
+        throw new UnsupportedOperationException("Unimplemented method 'getEnabledCategories'");
+    }
+}
+
